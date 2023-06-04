36b757dbcd7d14387347e97b27218473389bd482
shmvsky <shmvsky1337@gmail.com>
2023-06-05 00:06:56 +0300

    logging added

--- src/main/java/ru/shmvsky/chucknorrisamvera/ChuckController.java
+++ src/main/java/ru/shmvsky/chucknorrisamvera/ChuckController.java
@@ -1,6 +1,8 @@
 package ru.shmvsky.chucknorrisamvera;
 
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChuckController {
 
     private final Faker faker = new Faker();
+    private final Logger logger = LoggerFactory.getLogger(ChuckController.class);
 
     @GetMapping
     public Response getFact() {
+        logger.trace("http://localhost:8080/chuck-norris visited!");
         String fact = faker.chuckNorris().fact();
         return new Response(fact);
     }
